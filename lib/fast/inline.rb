1eb719ae35dc53d3a452dc745fa4d3aaa2752a82
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2019-03-01 23:21:28 -0300

    Support multiple local variable references

--- lib/fast/inline.rb
+++ lib/fast/inline.rb
@@ -10,17 +10,13 @@ module Fast
     module_function
     def local_variable code
       ast = Fast.ast(code)
-      lvars = *Fast.capture(ast, "$(lvasgn _ #simple")
-
       replacement = proc do |*nodes|
         nodes.each do |usage|
           variable_name, = usage.children
           lvar_node = Fast.capture(ast, "$(lvasgn #{variable_name})")
-          if index = lvars.index(variable_name)
-            source = lvars[index+1].loc.expression.source
-            replace(usage.loc.expression, source)
-            remove(lvar_node.loc.expression)
-          end
+          source = lvar_node.children.last.loc.expression.source
+          replace(usage.loc.expression, source)
+          remove(lvar_node.loc.expression)
         end
       end
       Fast.replace(ast, "(lvar _)", replacement)
--- spec/fast/inline_spec.rb
+++ spec/fast/inline_spec.rb
@@ -10,10 +10,18 @@ RSpec.describe Fast::Inline do
         is_expected.to eq(";1 + 1")
       end
     end
+
     context "complex expression" do
       subject { described_class.local_variable("a = 1;a + a") }
       it "inline local variable references with the current expression" do
         is_expected.to eq(";1 + 1")
+      end
+    end
+
+    context "multiple refences" do
+      subject { described_class.local_variable("a = 1;b = 2;a + b") }
+      it "inline multiple local variable references with the current expression" do
+        is_expected.to eq(";;1 + 2")
       end
     end
   end
