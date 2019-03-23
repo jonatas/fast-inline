396a6ca0d266d334dd90dc8df85ea0c0f87afa5d
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2019-03-23 19:25:34 -0300

    Format code

--- lib/fast/inline.rb
+++ lib/fast/inline.rb
@@ -1,5 +1,7 @@
-require "fast/inline/version"
-require "fast"
+# frozen_string_literal: true
+
+require 'fast/inline/version'
+require 'fast'
 
 def simple(node)
   node.children.size == 1
@@ -8,7 +10,8 @@ end
 module Fast
   module Inline
     module_function
-    def local_variable code
+
+    def local_variable(code)
       ast = Fast.ast(code)
       replacement = proc do |*nodes|
         nodes.each do |usage|
@@ -19,25 +22,43 @@ module Fast
           remove(lvar_node.loc.expression)
         end
       end
-      Fast.replace(ast, "(lvar _)", replacement)
+      Fast.replace(ast, '(lvar _)', replacement)
     end
 
-    def method_def code
+    def method_def(code)
       ast = Fast.ast(code)
-      methods= Fast.search(ast, "(def _ (args) )")
-      method_names = methods.map{|n|n.children[0]}
-      method_usages = "(send nil { #{ method_names.join(' ') } })"
+      methods = Fast.search(ast, '(def _ (args) )')
+      method_names = methods.map { |n| n.children[0] }
+      method_usages = "(send nil { #{method_names.join(' ')} })"
 
       replacement = proc do |*usages|
         usages.each do |usage|
-          def_node = Fast.search(ast, "(def #{usage.children.last} (args) $...)").first
+          def_node = Fast.search(ast, "(def #{usage.children[1]} (args) ...)").first
           def_body = def_node.children.last
           source = def_body.loc.expression.source
-          replace(usage.loc.expression, source)
+
           remove(def_node.loc.expression)
+          replace(usage.loc.expression, source)
         end
       end
-      Fast.replace(ast, method_usages, replacement)
+
+      output = Fast.replace(ast, method_usages, replacement)
+      ast = Fast.ast(code)
+
+      usages = Fast.search(ast, method_usages)
+      args = Fast.capture(ast, '(args (arg $_))')
+      args = [*args]
+
+      arguments_bind = [usages.first.children.last]
+      params_inline = args.each_with_object({}) do |arg, h|
+        h[arg] = arguments_bind.shift.loc.expression.source
+      end
+      arg_usage = Fast.search(ast, "(lvar { #{args.join(' ')}  })")
+      arg_usage.each do |arg_call|
+        arg_name = arg_call.children.last
+        output.gsub!(arg_call.loc.expression.source, params_inline[arg_name])
+      end
+      output
     end
   end
 end
--- spec/fast/inline_spec.rb
+++ spec/fast/inline_spec.rb
@@ -1,45 +1,46 @@
+# frozen_string_literal: true
+
 RSpec.describe Fast::Inline do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Fast::Inline::VERSION).not_to be nil
   end
 
-  describe ".local_variable" do
-    context "simple expression" do
-      subject { described_class.local_variable("a = 1;a + 1") }
-      it "inline local variable with the current expression" do
-        is_expected.to eq(";1 + 1")
+  describe '.local_variable' do
+    context 'simple expression' do
+      subject { described_class.local_variable('a = 1;a + 1') }
+      it 'inline local variable with the current expression' do
+        is_expected.to eq(';1 + 1')
       end
     end
 
-    context "complex expression" do
-      subject { described_class.local_variable("a = 1;a + a") }
-      it "inline local variable references with the current expression" do
-        is_expected.to eq(";1 + 1")
+    context 'complex expression' do
+      subject { described_class.local_variable('a = 1;a + a') }
+      it 'inline local variable references with the current expression' do
+        is_expected.to eq(';1 + 1')
       end
     end
 
-    context "multiple refences" do
-      subject { described_class.local_variable("a = 1;b = 2;a + b") }
-      it "inline multiple local variable references with the current expression" do
-        is_expected.to eq(";;1 + 2")
+    context 'multiple refences' do
+      subject { described_class.local_variable('a = 1;b = 2;a + b') }
+      it 'inline multiple local variable references with the current expression' do
+        is_expected.to eq(';;1 + 2')
       end
     end
   end
 
-  describe ".method_def" do
-    context "without arguments" do
-      subject { described_class.method_def("def one; 1 end; one") }
-      it "inline method definition with the current method call" do
-        is_expected.to eq("; 1")
+  describe '.method_def' do
+    context 'without arguments' do
+      subject { described_class.method_def('def one; 1 end; one') }
+      it 'inline method definition with the current method call' do
+        is_expected.to eq('; 1')
       end
     end
-=begin
-    context "arguments in method" do
-      subject { described_class.method_def("def plus_one number; number + 1 end; plus_one(1)") }
-      it "inline multiple local variable references with the current expression" do
-        is_expected.to eq(";;1 + 1")
+
+    context 'arguments in method' do
+      subject { described_class.method_def('def plus_one number; number + 1 end; plus_one(1)') }
+      it 'inline multiple local variable references with the current expression' do
+        is_expected.to eq('; 1 + 1')
       end
     end
-=end
   end
 end
